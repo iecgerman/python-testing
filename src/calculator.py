3ae1fc7d40d50f1ee6720517fdc1c869587805f3
iecgerman <iec_german@hotmail.com>
2024-10-02 02:46:30 -0600

    Clase 15 - Documentación de pruebas unitarias con Doctest en Python

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,14 @@
 def sum(a,b):
+
+    """
+    # sesion interactiva
+    >>> sum(5,7)
+    12
+
+    >>> sum(4,-4)
+    0
+    """
+
     return a + b
 
 def subtract(a,b):
@@ -8,6 +18,11 @@ def multiply(a,b):
     return a * b
 
 def divide(a,b):
+    """
+    >>> divide(10,0)
+    Traceback (most recent call last):
+    ValueError: La division por cero no esta permitida
+    """
     if b == 0:
         raise ValueError("La division por cero no esta permitida")
     return a / b
